e203132dc5301ed8b6499bba55437e0ac30ca09b
Slim Abida <slimabida21@gmail.com>
2026-02-05 01:14:51 +0100

    Feat: intégration de la section meileurs vente 1-ajout la props bestseller dans les props du composant ProductItem 2 - appler la props dans le composant BestSeller.jsx en la rajoutant dans les props mapper ProductItem pour affichage des articles

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useContext } from "react";
+import { useState, useEffect } from "react";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const BestSeller = () => {
+  // Importation du ShopContext via le useContext//
+  const { products } = useContext(ShopContext);
+
+  // State //
+  const [bestSellerProducts, setBestSellerProducts] = useState([]);
+
+  useEffect(() => {
+    setBestSellerProducts(products.filter((product) => product.bestseller));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"Meilleurs"} text2={"Ventes"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Découvrez les pièces qui vous font craquer : explorez nos meilleures
+          ventes et trouvez vos futurs favoris.
+        </p>
+      </div>
+      {/* Rendu des Produits  */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {bestSellerProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+            bestseller={item.bestseller}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { Link } from "react-router-dom";
 
-const ProductItem = ({ id, image, name, price }) => {
+const ProductItem = ({ id, image, name, price, bestseller }) => {
   const { currency } = useContext(ShopContext);
 
   return (
